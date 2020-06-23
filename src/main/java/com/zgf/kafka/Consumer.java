3eb4e1bc3f5ca1767a9c0c1f94b1722afa47c842
zgf <01117498@wisedu.com>
2020-06-23 16:47:09 +0800

    feat: 部分属性定义

--- src/main/java/com/zgf/kafka/Consumer.java
+++ src/main/java/com/zgf/kafka/Consumer.java
@@ -14,16 +14,24 @@ public class Consumer {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
+        // brokers 地址
         properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("group.id", "jd-group");
+        // 指定该 consumer 将加入的消费组
+        properties.put("group.id", "zgf-group");
+        // 开启自动提交 offset
         properties.put("enable.auto.commit", "true");
         properties.put("auto.commit.interval.ms", "1000");
+
         properties.put("auto.offset.reset", "latest");
         properties.put("session.timeout.ms", "30000");
+        properties.put("heartbeat.interval.ms", "10000");
+        properties.put("max.poll.interval.ms", "100000");
+        // 指定序列化类
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        // 订阅消费主题
         kafkaConsumer.subscribe(CollUtil.newArrayList(topic));
         while (true) {
             ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
